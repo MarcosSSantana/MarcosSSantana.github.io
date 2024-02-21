2059625a769de3fbd09d505db8c9d68a4fd55d00
Marcos Santana <marcosshotokan10@gmail.com>
2024-02-21 17:08:26 -0300

    add cartela-raspadinha

---
+++ cartela-raspadinha/assets/config.js
@@ -0,0 +1,213 @@
+
+//VOLUME MUSICA DE FUNDO
+const cfMusicSound = {
+    volume: 0.1,
+}
+//VOLUME MUSICA DOS PREMIOS
+const cfWinSound = {
+    volume: 0.1,
+}
+//VOLUME MUSICA MAIS CREDITOS
+const cfWinMcr = {
+    volume: 0.09,
+}
+//VOLUME MUSICA NOVA CHANCE
+const cfWinNch = {
+    volume: 0.09,
+}
+//VOLUME MUSICA BONUS
+const cfWinBns = {
+    volume: 0.09,
+}
+//TEXTO LOADING 
+const cfTextLoading = {
+    fontFamily: 'Arial',
+    fill: ['#ffffff'],
+    fontSize: 50,
+    fontWeight: 'bold',
+    lineJoin: 'round',
+    strokeThickness: 12,
+}
+
+const Cf = {
+    H: {
+        background: {
+            image: 'backgroundGameH',
+        },
+        cartela: [
+            {
+                x: 60,
+                y: 255
+            },
+            {
+                x: 501,
+                y: 255
+            },
+
+        ],
+        btnPlay: {
+            x: 1015,
+            y: 490
+        },
+        btnAuto: {
+            x: 1082,
+            y: 398
+        },
+        btnAjuda: {
+            x: 1082,
+            y: 276
+        },
+        btnComprar: {
+            x: 1073,
+            y: 145
+        },
+        btnCoin: {
+            x: 1081,
+            y: 31
+        },
+        btnCloseModal: {
+            x: 1200,
+            y: 31
+        },
+        credito: {
+            x: 90,
+            y: 663
+        },
+        premio: {
+            x: 874,
+            y: 663
+        },
+        id: {
+            x: 1254,
+            y: 180
+        },
+        tituloModals:{
+            x:640,
+            y:70
+        }
+    },
+    V: {
+        background: {
+            image: 'backgroundGameV',
+        },
+        cartela: [
+            {
+                x: 165,
+                y: 244
+            },
+            {
+                x: 165,
+                y: 628
+            },
+
+        ],
+        btnPlay: {
+            x: 255,
+            y: 1015
+        },
+        btnAuto: {
+            x: 502,
+            y: 1109
+        },
+        btnAjuda: {
+            x: 604,
+            y: 1109
+        },
+        btnComprar: {
+            x: 145,
+            y: 1100
+        },
+        btnCoin: {
+            x: 40,
+            y: 1108
+        },
+        btnCloseModal: {
+            x: 630,
+            y: 1000
+        },
+        credito: {
+            x: 140,
+            y: 1213
+        },
+        premio: {
+            x: 612,
+            y: 1213
+        },
+        id: {
+            x: 20,
+            y: 880
+        },
+        tituloModals:{
+            x:360,
+            y:70
+        }
+    },
+    itensCard: [
+
+        [
+            {
+                id: 0,
+                x: 50,
+                y: 50
+            },
+            {
+                id: 1,
+                x: 50,
+                y: 140
+            },
+            {
+                id: 2,
+                x: 50,
+                y: 230
+            },
+            {
+                id: 3,
+                x: 228,
+                y: 50
+            }, {
+                id: 4,
+                x: 228,
+                y: 140
+            },
+            {
+                id: 5,
+                x: 228,
+                y: 230
+            }
+        ],
+        [
+            {
+                id: 0,
+                x: 50,
+                y: 50
+            },
+            {
+                id: 1,
+                x: 50,
+                y: 140
+            },
+            {
+                id: 2,
+                x: 50,
+                y: 230
+            },
+            {
+                id: 3,
+                x: 228,
+                y: 50
+            }, {
+                id: 4,
+                x: 228,
+                y: 140
+            },
+            {
+                id: 5,
+                x: 228,
+                y: 230
+            }
+        ],
+
+
+
+    ]
+}
